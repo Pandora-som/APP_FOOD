adc258de200f920520f43025e28ea1b7ac6090e2
kuratova_ae <sofiya.katre@bk.ru>
2024-04-04 10:26:47 +0500

    Создана главная страница со столами для администратора

---
+++ AdminPage1.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NutriPlan
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminPage1.xaml
+    /// </summary>
+    public partial class AdminPage1 : Window
+    {
+        public AdminPage1()
+        {
+            InitializeComponent();
+        }
+    }
+}
