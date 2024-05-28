3d067396863873bcc9634c533651fd666591e13b
Maria <testirovsik0005mail.ru>
2024-05-28 19:33:52 +0500

    Проверка

--- AdminPage1.xaml.cs
+++ AdminPage1.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace NutriPlan
 {
@@ -23,21 +16,28 @@ namespace NutriPlan
         public AdminPage1()
         {
             InitializeComponent();
+            CountUsers();
             ShowAddDishes(Visibility.Hidden, Visibility.Visible, Visibility.Hidden);
+            TimerUser();
         }
+
+        List<ReferenceItem> indgredients = new List<ReferenceItem>();
+        List<ReferenceItem> ingridChoise = new List<ReferenceItem>();
 
 
         private void NewDish(object sender, MouseButtonEventArgs e)
         {
             ShowAddDishes(Visibility.Visible, Visibility.Hidden, Visibility.Hidden);
+            Addinfo();
         }
 
         private void TextBlock_MouseUp(object sender, MouseButtonEventArgs e)
         {
             ShowAddDishes(Visibility.Visible, Visibility.Hidden, Visibility.Hidden);
+            Addinfo();
         }
 
-        private void ShowAddDishes (Visibility addDishes, Visibility tables, Visibility dishesTable)
+        private void ShowAddDishes(Visibility addDishes, Visibility tables, Visibility dishesTable)
         {
             PanelAddDishes.Visibility = addDishes;
             PanelTables.Visibility = tables;
@@ -53,5 +53,97 @@ namespace NutriPlan
         {
             ShowAddDishes(Visibility.Hidden, Visibility.Visible, Visibility.Hidden);
         }
-    }
+
+        private void exit_click(object sender, MouseButtonEventArgs e)
+        {
+            MainWindow toUseWindow = new MainWindow();
+            toUseWindow.Show();
+            this.Close();
+        }
+
+        private void CountUsers()
+        {
+            int countRows = 0;
+            dbutils.connectDB = dbutils.GetDataDBConnection();
+            dbutils.connectDB.Open();
+            try
+            {
+                using (MySqlDataReader reader = dbutils.QueryDB(string.Format("SELECT COUNT(*) FROM {0}", dbutils.TableUsers)).ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            countRows = reader.GetInt32(0);
+                            UpdateUsers.Text = countRows.ToString();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex);
+            }
+            finally
+            {
+                dbutils.connectDB.Close();
+                dbutils.connectDB.Dispose();
+            }
+        }
+
+        DispatcherTimer timer = null;
+
+        void TimerUser()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(900);
+            timer.Tick += new EventHandler(timer_Elapsed);
+            timer.Start();
+        }
+        void timer_Elapsed(object sender, EventArgs e)
+        {
+            CountUsers();
+        }
+
+        private void Save_btnDishBD(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Addinfo()
+        {
+            IngredientsBDSearch.Items.Clear();
+            dbutils.connectDB = dbutils.GetDataDBConnection();
+            dbutils.connectDB.Open();
+            try
+            {
+                dbutils.LoadReference(dbutils.TableIngridients, indgredients, IngredientsBDSearch);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex, "Error");
+            }
+            finally
+            {
+                dbutils.connectDB.Close();
+                dbutils.connectDB.Dispose();
+            }
+        }
+        private void Add_btn_create(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("It's me, Carl!");
+            ingridChoise.Add(indgredients[IngredientsBDSearch.SelectedIndex]);
+            IngredientsBDADD.Items.Clear();
+            for (int i = 0; i < indgredients.Count; i++)
+            {IngredientsBDADD.Items.Add(indgredients[i].Name);}
+        }
+
+        private void Delete_btn_create(object sender, MouseButtonEventArgs e)
+        {
+            ingridChoise.Add(indgredients[IngredientsBDSearch.SelectedIndex]);
+            IngredientsBDADD.Items.Clear();
+            for (int i = 0; i < indgredients.Count; i++)
+            { IngredientsBDADD.Items.Add(indgredients[i].Name); }
+        }
+}
 }
---
+++ dbutils.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Controls;
+
+namespace NutriPlan
+{
+    class dbutils
+    {
+        public static MySqlConnection connectDB { set; get; }
+        public static string TableAdmin = "admins";
+        public static string TableAnswerVariant = "answer_variant";
+        public static string TableDishes = "dishes";
+        public static string TableIngridients = "ingredients";
+        public static string TableIngridientsDish = "ingredients_dishes";
+        public static string TableMeal = "meal_type";
+        public static string TableMenu = "menu";
+        public static string TableMenuDishes = "menu_dishes";
+        public static string TableTab = "tables";
+        public static string TableTypesDishes = "types_of_dishes";
+        public static string TableTypesTables = "types_of_tables";
+        public static string TableUsers = "users";
+        public static DBConnectString connectString = new DBConnectString();
+
+        public static MySqlConnection GetDBConnection(string host, int port, string database, string username, string password)
+        {
+            String connString = String.Format("Server={0};Database={1};port={2};User Id={3};password={4};convert zero datetime=True", host, database, port, username, password);
+            MySqlConnection conn = new MySqlConnection(connString);
+            return conn;
+        }
+
+        public static MySqlCommand QueryDB(string query)
+        {
+            MySqlCommand cmd = connectDB.CreateCommand();
+            cmd.CommandText = query;
+            return cmd;
+        }
+
+        public static MySqlConnection GetDataDBConnection()
+        {
+            string host = "localhost";
+            int port = 3306;
+            string database = "nutriplan";
+            string username = "viewer";
+            string password = "GfhjkmJnUs88";
+
+            return GetDBConnection(host, port, database, username, password);
+        }
+
+        public static void LoadReference(string TableName, List<ReferenceItem> list, ComboBox comboBox)
+        {
+            using (MySqlDataReader reader = dbutils.QueryDB("SELECT * FROM " + TableName).ExecuteReader())
+            {
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new ReferenceItem(reader.GetInt32(0), reader.GetString(1)));
+                    }
+                }
+            }
+            if (comboBox != null)
+            {
+                foreach (ReferenceItem item in list)
+                {
+                    comboBox.Items.Add(item.Name);
+                }
+            }
+        }
+    }
+
+    class DBConnectString
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string Database { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public DBConnectString()
+        {
+            Host = "localhost";
+            Port = 3306;
+            Database = "nutriplan";
+            Username = "viewer";
+            Password = "GfhjkmJnUs88";
+        }
+    }
+
+    public class ReferenceItem
+    {
+        public int ID { set; get; }
+        public string Name { set; get; }
+        public ReferenceItem(int id, string name)
+        {
+            ID = id;
+            Name = name;
+        }
+    }
+}
