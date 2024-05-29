41e6edbd3a57bb89aea660f425d9c35e4915c7b4
kuratova_ae <sofiya.katre@bk.ru>
2024-05-29 16:02:28 +0500

    Изменения за 29 мая

--- AdminPage1.xaml.cs
+++ AdminPage1.xaml.cs
@@ -1,17 +1,11 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace NutriPlan
 {
@@ -23,21 +17,28 @@ namespace NutriPlan
         public AdminPage1()
         {
             InitializeComponent();
+            CountUsers();
             ShowAddDishes(Visibility.Hidden, Visibility.Visible, Visibility.Hidden);
+            TimerUser();
         }
+
+        List<ReferenceItem> indgredients = new List<ReferenceItem>();
+        List<ReferenceItem> ingridChoise = new List<ReferenceItem>();
 
 
         private void NewDish(object sender, MouseButtonEventArgs e)
         {
             ShowAddDishes(Visibility.Visible, Visibility.Hidden, Visibility.Hidden);
+            Addinfo();
         }
 
         private void TextBlock_MouseUp(object sender, MouseButtonEventArgs e)
         {
             ShowAddDishes(Visibility.Visible, Visibility.Hidden, Visibility.Hidden);
+            Addinfo();
         }
 
-        private void ShowAddDishes (Visibility addDishes, Visibility tables, Visibility dishesTable)
+        private void ShowAddDishes(Visibility addDishes, Visibility tables, Visibility dishesTable)
         {
             PanelAddDishes.Visibility = addDishes;
             PanelTables.Visibility = tables;
@@ -52,6 +53,120 @@ namespace NutriPlan
         private void Closetabe(object sender, MouseButtonEventArgs e)
         {
             ShowAddDishes(Visibility.Hidden, Visibility.Visible, Visibility.Hidden);
+        }
+
+        private void exit_click(object sender, MouseButtonEventArgs e)
+        {
+            MainWindow toUseWindow = new MainWindow();
+            toUseWindow.Show();
+            this.Close();
+        }
+
+        private void CountUsers()
+        {
+            int countRows = 0;
+            dbutils.connectDB = dbutils.GetDataDBConnection();
+            dbutils.connectDB.Open();
+            try
+            {
+                using (MySqlDataReader reader = dbutils.QueryDB(string.Format("SELECT COUNT(*) FROM {0}", dbutils.TableUsers)).ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            countRows = reader.GetInt32(0);
+                            UpdateUsers.Text = countRows.ToString();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex);
+            }
+            finally
+            {
+                dbutils.connectDB.Close();
+                dbutils.connectDB.Dispose();
+            }
+        }
+
+        DispatcherTimer timer = null;
+
+        void TimerUser()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(900);
+            timer.Tick += new EventHandler(timer_Elapsed);
+            timer.Start();
+        }
+        void timer_Elapsed(object sender, EventArgs e)
+        {
+            CountUsers();
+        }
+
+        private void Save_btnDishBD(object sender, MouseButtonEventArgs e)
+        {
+            dbutils.connectDB = dbutils.GetDataDBConnection();
+            dbutils.connectDB.Open();
+
+            try
+            {
+                MySqlCommand cmd = dbutils.QueryDB("INSERT INTO " + dbutils.TableDishes + "(Name, Energy_value, Type_of_dish, Meal_type) VALUES (@name,@energy,@typeDish,@typeMeal)");
+                cmd.Parameters.Add("@name", MySqlDbType.VarChar).Value = nameDish.Text;
+                cmd.Parameters.Add("@energy", MySqlDbType.Float).Value = float.Parse(kkl.Text);
+                cmd.Parameters.Add("@typeDish", MySqlDbType.Int32).Value = 0;//сюда соответственно число
+                cmd.Parameters.Add("@typeMeal", MySqlDbType.Int32).Value = 0;//сюда соответственно число
+                cmd.ExecuteNonQuery(); //Это выполнение запроса
+                long lastId = cmd.LastInsertedId; //Получаем ID вставленного блюда
+                MessageBox.Show("Блюдо добавлено.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex);
+            }
+            finally
+            {
+                dbutils.connectDB.Close();
+                dbutils.connectDB.Dispose();
+            }
+        }
+
+        private void Addinfo()
+        {
+            IngredientsBDSearch.Items.Clear();
+            dbutils.connectDB = dbutils.GetDataDBConnection();
+            dbutils.connectDB.Open();
+            try
+            {
+                dbutils.LoadReference(dbutils.TableIngridients, indgredients, IngredientsBDSearch);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex, "Error");
+            }
+            finally
+            {
+                dbutils.connectDB.Close();
+                dbutils.connectDB.Dispose();
+            }
+        }
+
+        private void Add_btn_create(object sender, RoutedEventArgs e)
+        {
+            ingridChoise.Add(indgredients[IngredientsBDSearch.SelectedIndex]);
+            IngredientsBDADD.Items.Clear();
+            for (int i = 0; i < ingridChoise.Count; i++)
+            { IngredientsBDADD.Items.Add(ingridChoise[i].Name); }
+        }
+
+        private void Delete_btn_create(object sender, RoutedEventArgs e)
+        {
+            ingridChoise.RemoveAt(IngredientsBDADD.SelectedIndex);
+            IngredientsBDADD.Items.Clear();
+            for (int i = 0; i < ingridChoise.Count; i++)
+            {IngredientsBDADD.Items.Add(ingridChoise[i].Name);}
         }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -23,6 +26,34 @@ namespace NutriPlan
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void signin_btn(object sender, MouseButtonEventArgs e)
+        {
+            dbutils.connectDB = dbutils.GetDataDBConnection();
+            dbutils.connectDB.Open();
+            try
+            {
+                using (MySqlDataReader reader = dbutils.QueryDB(string.Format("SELECT * FROM {0} WHERE Login = '{1}' AND Password = '{2}'", dbutils.TableAdmin, textBoxUserName.Text, textBoxPass.Text)).ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        reader.Read();
+                        AdminPage1 toUseWindow = new AdminPage1();
+                        toUseWindow.Show();
+                        this.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex);
+            }
+            finally
+            {
+                dbutils.connectDB.Close();
+                dbutils.connectDB.Dispose();
+            }
         }
     }
 }
